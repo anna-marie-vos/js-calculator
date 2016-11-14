3b2562aa4264091ffbd0b89c247b3820a02f8e1c
anna-marie-vos <amvos@outlook.com>
2016-11-14 20:25:44 +1300

    high level strategy added to script file

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ $(document).ready(mainProgram);
 function mainProgram(){
 
 }
+// take input from all buttons
+// subtract operation
+// addition operation
+// subtraction operation
+// division operation
+// answer operation
+// percentage operation
+// clear items entered
+// clear memory
