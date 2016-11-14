737ee55e401991b7578814990ff0690b3ff32b04
anna-marie-vos <amvos@outlook.com>
2016-11-15 10:03:55 +1300

    preliminary calculation tracker function added

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ $(document).ready(mainProgram);
 
 function mainProgram(){
 
+document.addEventListener("click",assignValues);
+
 }
+var recordingString="";
 // take input from all buttons
+//01: add event listeners for all buttons
+function assignValues(evt){
+var x = evt.target.innerHTML;
+recordingString += x;
+record();
+}
+// function assignOperators(evt){
+//
+// }
+// function assignMemory(evt){
+//
+// }
 // subtract operation
 // addition operation
 // subtraction operation
@@ -13,4 +28,7 @@ function mainProgram(){
 // clear items entered
 // clear memory
 //show calculations up to now.
+function record(){
+  document.getElementById('tracker').innerHTML = recordingString;
+}
 // use API for scientific operations
