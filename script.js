df738e7bf7494bc994cd2c4a4cbac687a3a05971
anna-marie-vos <amvos@outlook.com>
2016-11-15 10:57:32 +1300

    operator record added

--- script.js
+++ script.js
@@ -2,20 +2,43 @@ $(document).ready(mainProgram);
 
 function mainProgram(){
 
-document.addEventListener("click",assignValues);
+clickNumbers(document.getElementsByClassName('inputs')[0].children)
+clickOperators(document.getElementsByClassName('operators')[0].children)
 
 }
 var recordingString="";
+var values = " ";
 // take input from all buttons
-//01: add event listeners for all buttons
-function assignValues(evt){
-var x = evt.target.innerHTML;
-recordingString += x;
-record();
+//01: add event listeners for all numbers
+function clickNumbers(numbers){
+  for(var a =0; a<numbers.length; a++){
+    numbers[a].addEventListener('click',assignValues);
+  }
 }
-// function assignOperators(evt){
-//
-// }
+// keep record of the values.
+//convert the value to a number
+function assignValues(evt){
+  var x = evt.target.innerHTML;
+  recordingString += x;
+  values +=x;
+  var output = parseFloat(values);
+  record();
+  console.log (output);
+  return output;
+}
+//add event listeners for the operators
+function clickOperators(operator){
+  for(var a =0; a<operator.length; a++){
+    operator[a].addEventListener('click',assignOperators);
+  }
+}
+//keep record of all the operators
+//assign a value to them.
+function assignOperators(evt){
+  var x = evt.target.innerHTML;
+  recordingString += x;
+  record();
+}
 // function assignMemory(evt){
 //
 // }
