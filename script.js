5d9d63f2cadb30ca95d3e176b4c050d522e2a02c
anna-marie-vos <amvos@outlook.com>
2016-11-15 18:26:12 +1300

    comments added to the code, to keep track of my thoughts

--- script.js
+++ script.js
@@ -18,18 +18,20 @@ function clickNumbers(numbers){
   }
 }
 // keep record of the values.
-//convert the value to a number
+//accumulate the string of values until another operation is pressed.
 function assignValues(evt){
   var x = evt.target.innerHTML;
   values +=x;
   record(x);
 }
+//convert the value string to a value.
+//and addit to the recordedEntries array.
+//and reset the values string for the new number.
 function recordValue(){
   recordedEntries.push(parseFloat(values));
-  console.log(values);
   resetValue();
 }
-//reset value
+//reset the values string for a new string to be added
 function resetValue(){
   values = "";
 }
@@ -40,27 +42,29 @@ function clickOperators(operator){
   }
 }
 //keep record of all the operators
-//assign a value to them.
+// record the last values
+//add the operator to the recordedEntries
+//and keep record of the operation
 function assignOperators(evt){
   recordValue();
   recordedEntries.push(evt.target.id);
   record(evt.target.innerHTML);
-  console.log(values);
-  console.log(recordedEntries);
 }
+//record the last values
+//record the last operation
+// display the answer
+//and perform a calculation
 function assignEqual(evt){
   recordValue();
   record(evt.target.innerHTML);
   answer(recordedEntries[0]);
-
   performcCalc();
 }
-//when the answer button is clicked perform find the operation and
-// use the values before and after it to do the calculation.
+//when the equal button is clicked perform find the operation and
+// use the values before and after it in the array to do the calculation.
 // send the answer to the html file.
 // and assign it to the last last number that was used.
 function performcCalc(){
-  console.log(recordedEntries);
   var y = 0;
   for(var x = 0; x< recordedEntries.length;x++){
     switch(recordedEntries[x]){
@@ -93,26 +97,24 @@ function performcCalc(){
       y = recordedEntries[x];
     }
   }
-
-  values = y;
   document.getElementById('tracker').innerHTML =y;
+  values = y;
   recordedEntries = [];
 }
-
+//clear the values
+//clear the recordedEntries
+//reset the html tracker and answer
 function clearAll(){
   values = ""
   recordedEntries = [];
   document.getElementById('tracker').innerHTML = "";
   document.getElementById('answer').innerHTML = "0";
 }
-
-// clear items entered
-// clear memory
-//show calculations up to now.
+//add the either the value or operator the html tracker
 function record(x){
   document.getElementById('tracker').innerHTML += x;
 }
+//send the value to the answer html
 function answer(x){
   document.getElementById('answer').innerHTML = x;
 }
-// use API for scientific operations
